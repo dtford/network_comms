0d04c6d4d94aa72cd8ca58b711d04c791481ef12
Dallen Ford <forddt@unm.edu>
2021-06-24 23:20:34 -0400

    Processing CLI args.

--- comms_args.cpp
+++ comms_args.cpp
@@ -1,16 +1,84 @@
 #include "comms_args.h"
+#include <iostream>
 
 CommsArgsReader::CommsArgsReader(int argc, char **argv)
 {
+	// default
+	command = ERROR;
+
 	//for testing only
 	if( argc >= 2)
 	{
-		url = argv[1];
+		for(int arg_index = 1; arg_index < argc; ++arg_index)
+		{
+			std::string arg = argv[arg_index];
+			if(arg == "-u" || arg == "--url")
+			{
+				// read url
+				if( url.empty() && ( ++arg_index < argc ))
+				{
+					url = argv[arg_index];
+				}
+				else
+				{
+					command = ERROR;
+					break;
+				}	
+			}
+			else if(arg == "-o" || arg == "--post")
+			{
+				command = POST;
+			}
+			else if(arg == "-g" || arg == "--get")
+			{
+				command = GET;
+			}
+			else if(arg == "-p" || arg == "--put")
+			{
+				command = PUT;
+			}
+			else if(arg == "-d" || arg == "--delete")
+			{
+				command = DELETE;
+			}
+			else if(arg == "-h" || arg == "--help")
+			{
+				command = HELP;
+			}
+			else
+			{
+				if( !url.empty() && command != ERROR )
+				{
+					// read the rest as the freetext string
+					freeText = arg;
+					for( ++arg_index; arg_index < argc; ++arg_index)
+					{
+						freeText.append( " " );
+						freeText.append( argv[arg_index] );
+					}
+				}
+				else
+				{
+					command = ERROR;
+					break;
+				}
+			}
+		}
 	}
 	else
 	{
 		command = ERROR;
-	}	
+	}
+	if( ( ( command == PUT ) || ( command == POST ) ) && freeText.empty() )
+	{
+		// post and put require a string, so this is an error
+		command = ERROR;
+	}
+	if( ( command != ERROR ) && ( command != HELP ) && ( url.empty() ) )
+	{
+		// request commands require a url, so this is an error
+		command = ERROR;
+	}
 }
 
 std::string CommsArgsReader::getURL()
--- comms_main.cpp
+++ comms_main.cpp
@@ -8,13 +8,39 @@ int main(int argc, char **argv)
 {
 	CommsArgsReader argsReader(argc, argv);
 	
-	// for testing only
-	if(argsReader.getCommand() != CommsArgsReader::ERROR )
+	switch(argsReader.getCommand() )
 	{
-		std::cout << argsReader.getURL() << "\n";
-	}
-	else
-	{
-		std::cout << "ERROR\n";
+		case CommsArgsReader::POST:
+		{
+			break;
+		}
+		case CommsArgsReader::GET:
+		{
+			break;
+		}
+		case CommsArgsReader::PUT:
+		{
+			break;
+		}
+		case CommsArgsReader::DELETE:
+		{
+			break;
+		}
+		case CommsArgsReader::ERROR:
+		{
+			std::cerr << "Input error.\n";
+		}
+		case CommsArgsReader::HELP:
+		{
+			std::cout << "Usage: " << argv[0] << " [command] [-u|--url URL [TEXT]]\n";
+			std::cout << "Commands/fields:\n";
+			std::cout << "  -d, --delete\t\tSend delete request. Requires URL.\n";
+			std::cout << "  -g, --get\t\tSend get request. Requires URL.\n";
+			std::cout << "  -h, --help\t\tShow this help message.\n";
+			std::cout << "  -o, --post\t\tSend post request. Requires URL and TEXT.\n";
+			std::cout << "  -p, --put\t\tSend put request. Requires URL and TEXT.\n";
+			std::cout << "  -u URL, --url URL\tSpecify URL for request.\n";
+			std::cout << "  TEXT\t\t\t free text field to be included in the request.\n";
+		}
 	}
 }
